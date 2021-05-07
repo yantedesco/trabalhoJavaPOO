6de01105b710334c84979e4c6ec1a2fd9850963a
brayancataldo <brayancataldo2016@gmail.com>
2021-05-06 22:14:30 -0300

    só tirei uma variavel inútil e apaguei uns espaços

--- src/banco/Relatorio.java
+++ src/banco/Relatorio.java
@@ -1,37 +1,22 @@
 package banco;
-
-
-
 public class Relatorio extends Conta {
-	private static double valorSimulado;
 	
-	
-	public Relatorio() {           }
-	
+	public Relatorio() {}
 	
 	
 	public static double simulaPoupanca(double valor, int dias) {
         double rend = 0.00533333;
         return rend / 30 * valor * dias;
-        
-       
-        
 	}
 	
-	
 	public void tributacao() {
-	
 	double totalM = (getTotalMovimentacoes()*taxaMovimentacao) ;
-	
 	double totalT = totalM  +  super.getTarifa();     
 	
 	System.out.println("O valor da tarifa cobrada é de  R$"+getTarifa());
 	System.out.println("O valor total das taxas de movimentaçãoo é de R$"+totalM);
 	System.out.println("O total das tributa�oes até agora é de = R$"+totalM);
+
 	
 	}
-	
-	
-	
-	
 }
